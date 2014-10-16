9489fa8beba58a67135a1274c23369c88f2dcf2b
GoesToEleven <todd.mcleod@fresnocitycollege.edu>
2014-10-16 14:04:46 -0700

    thursday

---
+++ 26_parse_object.js
@@ -0,0 +1,9 @@
+function parseObject(object) {
+    for (property in object) {
+        if (typeof property == "object") {
+            parseObject(object[property])
+        } else {
+            console.log(property + ": " + object[property])
+        }
+    }
+}
